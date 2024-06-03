87f21be29ca395c95a90c3cf54000184ee8075bf
abayellow <abayellow@github.com>
2024-06-04 10:05:43 +1100

    Initial Commit

---
+++ YndexAPITestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YndexAPITestTask
+//
+//  Created by Alexander Abanshin on 04.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
